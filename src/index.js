39e4bccbafe4bd3b50d5f65f9ae722186030bda3
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-07-13 21:46:32 +0200

    fix fontawesome issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from "react-router-dom" // Importation du RouterProvider de react-router
